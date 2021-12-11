c91972ebd31ac6a0b6e39650d0ce11e2587e2417
gretz <gretzinga@gmail.com>
2021-12-10 21:52:37 -0300

    labex

---
+++ labex/labex/labex/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>LabEx, viagens intergalácticas!</p>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ labex/labex/labex/src/pages/Home/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Home = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    height: 100vh;
+
+    `
